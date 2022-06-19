a9b3b21cd1fb1f18fee91ac1b6db4d65c3479e03
Cautreoxit <hukang20@mails.ucas.ac.cn>
2022-06-19 20:57:53 +0800

    sort directions

--- SortByMultipleKeywords/SortByMultipleKeywords.c
+++ SortByMultipleKeywords/SortByMultipleKeywords.c
@@ -9,6 +9,20 @@ int main(){
     int order[NUM_KEY];
     GetOrder(order);
 
+    int func;
+    while(1){
+        printf("请选择您想要使用的算法：(1/2/3)\n");
+        printf("1.LSD分配与收集\n");
+        printf("2.LSD稳定的内部排序法\n");
+        printf("3.MSD\n");
+        scanf("%d",&func);
+        if(func==1 || func==2 || func==3)
+            break;
+        printf("输入无效，请重新输入！\n");
+    }
+    
+    
+
     // 由随机数生成测试数据
     // 发现随机数在子函数里面生成会出现问题……于是就委曲求全在main函数里实现了
     int i, j;
@@ -27,19 +41,32 @@ int main(){
     printf("原始数据：\n");
     PrintSqList(SL);
 
-    // 进入算法
-    SqList *SL1=SL, *SL2=SL;
-    SL= LSDStable(SL1, order);
-    //SL2 = MSD(SL2, order);
-    LL  = LSDDistr(LL, order);
+    SqList *SL1=SL;
 
     // 把其中一个计算结果打印出来即可
-    printf("排序结果1：\n");
-    PrintLinkedList(LL);
-    printf("排序结果2：\n");
-    PrintSqList(SL);
+    //printf("排序结果1：\n");
+    //PrintLinkedList(LL);
+    //printf("排序结果2：\n");
+    //PrintSqList(SL);
     //printf("排序结果3：\n");
     //PrintSqList(SL2);
+
+    if(func==1){
+        LL  = LSDDistr(LL, order);
+        printf("排序结果1：\n");
+        PrintLinkedList(LL);
+    }
+    else if(func==2){
+        SL= LSDStable(SL1, order);
+        printf("排序结果2：\n");
+        PrintSqList(SL);
+    }
+    else{
+        SL= MSD(SL1, order);
+        printf("排序结果3：\n");
+        PrintSqList(SL);
+    }
+    
 
     system("pause");
 }
